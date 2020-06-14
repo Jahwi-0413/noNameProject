70e55be2ab07f636a5f0593876c214c3a5995713
CM-Kim <min1003k@gmail.com>
2020-06-14 16:08:04 +0900

    서버 동작 일부분 추가 / PR.read가 byte[]를 반환하도록 수정

--- src/server/PacketReader.java
+++ src/server/PacketReader.java
@@ -8,8 +8,12 @@ import tableClass.*;
 public class PacketReader {
     private static DBManager db = DBManager.getInstance();
 
-    public static void read(byte[] packet)
+    // 패킷을 읽어 적절한 작업 처리, 이후 전송 해야할 result 패킷을 반환
+    public static byte[] read(byte[] packet)
     {
+        boolean result = false;
+        Protocol protocol = null;
+
         switch(packet[0])
         {
             case Protocol.PW_USER:          // 권한이 사용자일때
@@ -20,15 +24,39 @@ public class PacketReader {
                         {
                             case Protocol.TAG_TRAVEL:   // 사용자의 관광지 조회
                                 ArrayList<Tour> tourList = db.getTourList();
-                                //TODO: list를 출력함수로 넘겨서 출력
+
+                                if(!tourList.isEmpty())
+                                {
+                                    //TODO: list를 출력함수로 넘겨서 출력
+                                    protocol = new Protocol(Protocol.PW_USER, Protocol.TYPE_SUCCESS, Protocol.PT_NULL, Protocol.PT_NULL);
+                                }
+                                else
+                                    protocol = new Protocol(Protocol.PW_USER, Protocol.TYPE_FAIL, Protocol.PT_NULL, Protocol.PT_NULL);
+                                return protocol.getPacket();
                                 break;
                             case Protocol.TAG_FOOD:     // 사용자의 식당 조회
                                 ArrayList<Restaurant> resList = db.getRestaurantList();
-                                //TODO: list를 출력함수로 넘겨서 출력
+
+                                if(!resList.isEmpty())
+                                {
+                                    //TODO: list를 출력함수로 넘겨서 출력
+                                    protocol = new Protocol(Protocol.PW_USER, Protocol.TYPE_SUCCESS, Protocol.PT_NULL, Protocol.PT_NULL);
+                                }
+                                else
+                                    protocol = new Protocol(Protocol.PW_USER, Protocol.TYPE_FAIL, Protocol.PT_NULL, Protocol.PT_NULL);
+                                return protocol.getPacket();
                                 break;
                             case Protocol.TAG_HOTEL:    // 사용자의 숙소 조회
                                 ArrayList<Lodgment> lodgList = db.getLodgmentList();
-                                //TODO: list를 출력함수로 넘겨서 출력
+
+                                if(!lodgList.isEmpty())
+                                {
+                                    //TODO: list를 출력함수로 넘겨서 출력
+                                    protocol = new Protocol(Protocol.PW_USER, Protocol.TYPE_SUCCESS, Protocol.PT_NULL, Protocol.PT_NULL);
+                                }
+                                else
+                                    protocol = new Protocol(Protocol.PW_USER, Protocol.TYPE_FAIL, Protocol.PT_NULL, Protocol.PT_NULL);
+                                return protocol.getPacket();
                                 break;
                         }
                         break;
@@ -43,15 +71,39 @@ public class PacketReader {
                         {
                             case Protocol.TAG_TRAVEL:   // 관리자의 관광지 조회
                                 ArrayList<Tour> tourList = db.getTourList();
-                                //TODO: list를 출력함수로 넘겨서 출력
+
+                                if(!tourList.isEmpty())
+                                {
+                                    //TODO: list를 출력함수로 넘겨서 출력
+                                    protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_SUCCESS, Protocol.PT_NULL, Protocol.PT_NULL);
+                                }
+                                else
+                                    protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_FAIL, Protocol.PT_NULL, Protocol.PT_NULL);
+                                return protocol.getPacket();
                                 break;
                             case Protocol.TAG_FOOD:     // 관리자의 식당 조회
                                 ArrayList<Restaurant> resList = db.getRestaurantList();
-                                //TODO: list를 출력함수로 넘겨서 출력
+
+                                if(!tourList.isEmpty())
+                                {
+                                    //TODO: list를 출력함수로 넘겨서 출력
+                                    protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_SUCCESS, Protocol.PT_NULL, Protocol.PT_NULL);
+                                }
+                                else
+                                    protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_FAIL, Protocol.PT_NULL, Protocol.PT_NULL);
+                                return protocol.getPacket();
                                 break;
                             case Protocol.TAG_HOTEL:    // 관리자의 숙소 조회
                                 ArrayList<Lodgment> lodgList = db.getLodgmentList();
-                                //TODO: list를 출력함수로 넘겨서 출력
+
+                                if(!tourList.isEmpty())
+                                {
+                                    //TODO: list를 출력함수로 넘겨서 출력
+                                    protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_SUCCESS, Protocol.PT_NULL, Protocol.PT_NULL);
+                                }
+                                else
+                                    protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_FAIL, Protocol.PT_NULL, Protocol.PT_NULL);
+                                return protocol.getPacket();
                                 break;
                         }
                         break;
@@ -59,13 +111,28 @@ public class PacketReader {
                         switch(packet[3])
                         {
                             case Protocol.TAG_TRAVEL:   // 관리자의 관광지 추가
-                                //TODO: 이곳에 동작 기술
+                                Tour tour = null;  //TODO: 패킷에서 바디부분 분리해서 역직렬화
+                                result = db.addTour(tour);
+
+                                if(result) protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_SUCCESS, Protocol.PT_NULL, Protocol.PT_NULL);
+                                else protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_FAIL, Protocol.PT_NULL, Protocol.PT_NULL);
+                                return protocol.getPacket();
                                 break;
                             case Protocol.TAG_FOOD:     // 관리자의 식당 추가
-                                //TODO: 이곳에 동작 기술
+                                Restaurant res = null;  //TODO: 패킷에서 바디부분 분리해서 역직렬화
+                                result = db.addRestaurant(res);
+
+                                if(result) protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_SUCCESS, Protocol.PT_NULL, Protocol.PT_NULL);
+                                else protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_FAIL, Protocol.PT_NULL, Protocol.PT_NULL);
+                                return protocol.getPacket();
                                 break;
                             case Protocol.TAG_HOTEL:    // 관리자의 숙소 추가
-                                //TODO: 이곳에 동작 기술
+                                Lodgment lodg = null;   //TODO: 패킷에서 바디부분 분리해서 역직렬화
+                                result = db.addLodgment(lodg);
+
+                                if(result) protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_SUCCESS, Protocol.PT_NULL, Protocol.PT_NULL);
+                                else protocol = new Protocol(Protocol.PW_ADMIN, Protocol.TYPE_FAIL, Protocol.PT_NULL, Protocol.PT_NULL);
+                                return protocol.getPacket();
                                 break;
                         }
                         break;
